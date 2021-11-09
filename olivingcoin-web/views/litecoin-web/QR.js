79a68f80f6741f6d2e7b41314cc9d05081a77356
CJwon-98 <cjone98692996@gmail.com>
2021-11-09 19:06:40 +0900

    결제 팝업창,음료 추가

--- olivingcoin-web/views/litecoin-web/QR.js
+++ olivingcoin-web/views/litecoin-web/QR.js
@@ -1,3 +1,10 @@
-function QROpen() {
-    window.open("PayQR.html", "결제창","width=420, height=500, left=100,top =100, scrollbars=no, location=no");
-}
+
+    $(document).ready(function(){
+        $("#paybutton").click(function(){
+            $("#QRpopup").fadeIn();
+        });
+        $("#QRpopup").click(function(){
+            $("#QRpopup").fadeOut();
+        });
+    });
+
